4bdbe8e5099ad7e5234b5c7dc928773b0d4ff860
Shweta Gadave <Shweta Gadave@LAPTOP-IAKU8825>
2023-09-12 22:08:40 +0530

    commit

---
+++ src/main/java/AutomationScripts/facegenie/App.java
@@ -0,0 +1,13 @@
+package AutomationScripts.facegenie;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
